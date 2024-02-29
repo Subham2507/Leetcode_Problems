861d1317a6888773bb2a2a964a1f41b052c24dce
Subham Ghosh <93569060+Subham2507@users.noreply.github.com>
2024-03-01 01:04:54 +0530

    Time: 39 ms (71.10%), Space: 16.7 MB (95.57%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -2,6 +2,6 @@ class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         dict1={}
         for num in arr:
-            dict1[num]=dict1.get(num,0)+1
+            dict1[num]=dict1.get(num,0)+1 ## dict.get(num,0) will return value corressponding to key num ... If there is nothing corressponding to num then it will return zero..
         return len(set(dict1.values()))==len(dict1.values())
         
