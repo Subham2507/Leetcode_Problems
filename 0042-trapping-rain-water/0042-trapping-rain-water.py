0e068eb4315e577d3c60667437878c6851e2d6f8
Subham Ghosh <93569060+Subham2507@users.noreply.github.com>
2024-03-07 23:23:50 +0530

    Time: 116 ms (22.40%), Space: 18.6 MB (36.43%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,11 +1,12 @@
 def left_max(height,n):
+    '''Find out the left max element for each number in array'''
     l1=[0 for x in range(n)]
     l1[0]=height[0]
     for i in range(1,n):
         l1[i]=max(l1[i-1],height[i])
-    #print(l1)
     return l1
 def right_max(height,n):
+    '''Find out the right max element for each number in array'''
     l2=[0 for x in range(n)]
     l2[n-1]=height[n-1]
     for i in range(n-2,-1,-1):
@@ -17,11 +18,9 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         n=len(height)
         s=0
-        l_left=left_max(height,n)
+        l_left=left_max(height,n) ## call the function
         l_right=right_max(height,n)
-        
-        #print(l_right)
-        for i in range(1,n-1):
-            s=s+min(l_right[i],l_left[i])-height[i]
+        for i in range(1,n-1):####As we don't need the boundary condition here
+            s=s+min(l_right[i],l_left[i])-height[i] ### volume of water for each width
         return s
         
