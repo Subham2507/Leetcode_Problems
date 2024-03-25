408181266da9f67e199b42195c47c71cee663a90
Subham Ghosh <93569060+Subham2507@users.noreply.github.com>
2024-03-25 23:04:23 +0530

    Time: 464 ms (20.22%), Space: 16.7 MB (60.08%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -2,13 +2,13 @@ class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         nums.sort()
         n=len(nums)
-        closest_sum=100000
-        for k in range(0,n-2):
+        closest_sum=100000 ### It should be greater than the range
+        for k in range(0,n-2): ##Simple doing 3sum 
             i=k+1
             j=n-1
             while(i<j):
                 sum_1=nums[i]+nums[j]+nums[k]
-                if (abs(target-sum_1))<(abs(target-closest_sum)):
+                if (abs(target-sum_1))<(abs(target-closest_sum)): ## Agar pichle se yea wala distance kam ho target se toh update karte chlo
                     closest_sum=sum_1
                 if(sum_1<target):
                     i=i+1
